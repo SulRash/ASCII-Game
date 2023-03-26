ce1fd53d7c657cae4f0d45954a1f0cd868bfc4b9
Sultan <92447824+SulRash@users.noreply.github.com>
2023-03-26 18:08:00 +0300

    Added tileset and testing grounds
    Added the tileset to be used for the creation of levels in the game.
    
    TODO: Fix collision bug where player goes through tilemap collisions.

--- src/player/player.gd
+++ src/player/player.gd
@@ -6,6 +6,27 @@ var defense
 var sanity
 var divinity
 var magic
+var velocity
+
+var speed = 100
+
+func _ready():
+	$CollisionShape2D.disabled = false
 
 func set_stats():
 	pass
+
+func _process(delta):
+	velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
